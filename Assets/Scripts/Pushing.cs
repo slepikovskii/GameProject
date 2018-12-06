e0695505308ae685d8a4a60d0b34d002dc54742a
ilixilia <ilixilia@gmail.com>
2018-12-06 14:45:26 +0300

    made tables and chairs pushable

---
+++ Assets/Scripts/Pushing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pushing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            this.GetComponent<Rigidbody>().AddForce(Vector3.forward*2f);
+        }
+    }
+}
